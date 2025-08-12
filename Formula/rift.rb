512a851bc9c8d394226ad69be687dd073b56fc07
Jack Smethurst <jack@newgenre.studio>
2025-08-12 16:06:27 +0100

    Update Homebrew formula version to 1.0.1 and adjust download URLs for macOS Intel and ARM architectures

--- Formula/rift.rb
+++ Formula/rift.rb
@@ -1,15 +1,15 @@
 class Rift < Formula
   desc "A Git alternative written in Go"
   homepage "https://github.com/jacksmethurst/rift-cli"
-  version "1.0.0"
+  version "1.0.1"
 
   on_macos do
     if Hardware::CPU.intel?
-      url "https://github.com/jacksmethurst/rift-cli/releases/download/v1.0.0/rift-darwin-amd64.tar.gz"
+      url "https://github.com/jacksmethurst/rift-cli/releases/download/v1.0.1/rift-darwin-amd64.tar.gz"
       sha256 "REPLACE_WITH_ACTUAL_SHA256_FOR_INTEL"
     end
     if Hardware::CPU.arm?
-      url "https://github.com/jacksmethurst/rift-cli/releases/download/v1.0.0/rift-darwin-arm64.tar.gz"
+      url "https://github.com/jacksmethurst/rift-cli/releases/download/v1.0.1/rift-darwin-arm64.tar.gz"
       sha256 "REPLACE_WITH_ACTUAL_SHA256_FOR_ARM"
     end
   end
